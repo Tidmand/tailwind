4f55acc4f46f1b7ade7a15a1854d1f033f555c5b
Søren Tidmand <media@tidmand.com>
2021-08-02 11:57:43 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      VitesseLight: ["Vitesse sans light, sans-serif"],
+      VitesseBook: ["Vitesse sans book, sans-serif"],
+      VitesseMedium: ["Vitesse sans medium, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
