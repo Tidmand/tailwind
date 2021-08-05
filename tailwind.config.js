e7ec5fbe1cd153af66f833acd3c8a6906db6ecdc
Søren Tidmand <media@tidmand.com>
2021-08-05 20:05:40 +0200

    Oprettelse af root default og to temaer, inkl. function til håndtering af variabler med opacity parameter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,40 @@
+function withOpacity(variableName) {
+  return ({opacityValue}) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        brand: "#9522FF",
-      }
+        brand: 'rgba(149,34,255,1.0)',
+      },
+      textColor: {
+        skin: {
+          base: withOpacity('--color-text-base'),
+          muted: withOpacity('--color-text-muted'),
+          inverted: withOpacity('--color-text-inverted'),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: withOpacity('--color-fill'),
+          'button-accent': withOpacity('--color-button-accent'),
+          'button-accent-hover': withOpacity('--color-button-accent-hover'),
+          'button-muted': withOpacity('--color-button-muted'),
+        },
+      },
+      gradientColorStops: {
+        skin: {
+          hue: withOpacity('--color-fill'),
+        },
+      },
     },
     fontFamily: {
       VitesseLight: ["Vitesse sans light, sans-serif"],
@@ -14,8 +43,8 @@ module.exports = {
     },
     container: {
       center: true,
-      padding: "1rem",
-    }
+      padding: '1rem',
+    },
   },
   variants: {
     extend: {
